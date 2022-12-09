200d2a843491ccfc4ff5ffc92974779c5ac34c0b
mikejmarin25 <105763252+mikejmarin25@users.noreply.github.com>
2022-12-08 22:03:10 -0500

    client file modified

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,2 +1,6 @@
 import  "./form";
 import "./submit";
+import Logo from '../images/logo.png';
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
+
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,8 +1,17 @@
 const path = require('path');
-  module.exports = {
-    entry: './src/js/index.js',
-    output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  }
   };
