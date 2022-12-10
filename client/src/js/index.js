ba9db71cdb7d4ec9b396287cbdbe348221b62355
mikejmarin25 <105763252+mikejmarin25@users.noreply.github.com>
2022-12-10 17:21:11 -0500

    css file added

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,20 @@
+// Import modules
 import  "./form";
 import "./submit";
+
+// Import CSS files
+import "../css/index.css";
+import "./custom.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+// Add images on load
+window.addEventListener('load', function () {
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+
 module.exports = {
   mode: 'development',
   entry: './src/js/index.js',
@@ -6,12 +7,29 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
   module: {
     rules: [
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: "defaults" }]
+          ]
+        }
+      }
+    }
     ]
   }
-  };
+}
