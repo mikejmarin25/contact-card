47753ec7d570980b1baa15d949f6ff04613cd957
mikejmarin25 <105763252+mikejmarin25@users.noreply.github.com>
2022-12-24 13:59:15 -0600

    code modified

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('../client/dist/'));
-//app.use(express.urlencoded({ extended: true }));
+app.use(express.static("../client/dist/"))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require('./routes/htmlRoutes.js')(app);
