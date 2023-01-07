e668240e5c75f6e156ce15640d397e3a7a50f95e
mikejmarin25 <105763252+mikejmarin25@users.noreply.github.com>
2023-01-07 17:57:58 -0500

    added database

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,11 +1,11 @@
 import "./form";
 import "./submit";
-import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
+
 
 window.addEventListener('load', function () {
     document.getElementById('logo').src = Logo;
